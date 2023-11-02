59dbfa808adf1ca9b0cefab4ba0013f2118ee6e3
Constantin Galus <ostapx79@gmail.com>
2023-11-02 18:36:50 +0300

    upgrade: src/main/*/ParrotTrouble.java

--- src/main/java/edu/coding_bat/warmup_1/parrot_trouble/ParrotTrouble.java
+++ src/main/java/edu/coding_bat/warmup_1/parrot_trouble/ParrotTrouble.java
@@ -2,8 +2,16 @@ package edu.coding_bat.warmup_1.parrot_trouble;
 
 public class ParrotTrouble {
     public boolean parrotTrouble(boolean talking, int hour) {
-        if (talking && (hour >= 0 && hour <= 6)) return true;
-        if (talking && (hour >= 21 && hour <= 23)) return true;
-        return false;
+
+        // Example 1
+        // if (talking && (hour >= 0 && hour <= 6)) return true;
+        // else if (talking && (hour >= 21 && hour <= 23)) return true;
+        // else return false;
+
+        // Example 2
+        // if ((talking && (hour >= 0 && hour <= 6)) || (talking && (hour >= 21 && hour <= 23))) return true;
+
+        // Example 3
+        return ((talking && (hour >= 0 && hour <= 6)) || (talking && (hour >= 21 && hour <= 23)));
     }
 }
