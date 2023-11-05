0b18d01b17b1d0c2ed6ce67afc677274d844a07b
Constantin Galus <ostapx79@gmail.com>
2023-11-05 14:17:06 +0300

    modified: src/test/*/Front3Test.java

--- src/test/java/edu/coding_bat/warmup_1/front3/Front3Test.java
+++ src/test/java/edu/coding_bat/warmup_1/front3/Front3Test.java
@@ -8,52 +8,52 @@ class Front3Test {
     Front3 fro3 = new Front3();
 
     @Test
-    public void front3_1() {
+    public void front3_1Test() {
         assertEquals(fro3.front3("java"), "javjavjav");
     }
 
     @Test
-    public void front3_2() {
+    public void front3_2Test() {
         assertEquals(fro3.front3("Chocolate"), "ChoChoCho");
     }
 
     @Test
-    public void front3_3() {
+    public void front3_3Test() {
         assertEquals(fro3.front3("abc"), "abcabcabc");
     }
 
     @Test
-    public void front3_4() {
+    public void front3_4Test() {
         assertEquals(fro3.front3("abcXYZ"), "abcabcabc");
     }
 
     @Test
-    public void front3_5() {
+    public void front3_5Test() {
         assertEquals(fro3.front3("ab"), "ababab");
     }
 
     @Test
-    public void front3_6() {
+    public void front3_6Test() {
         assertEquals(fro3.front3("a"), "aaa");
     }
 
     @Test
-    public void front3_7() {
+    public void front3_7Test() {
         assertEquals(fro3.front3(""), "");
     }
 
     @Test
-    public void front3_8() {
+    public void front3_8Test() {
         assertEquals(fro3.front3E(""), "");
     }
 
     @Test
-    public void front3_9() {
+    public void front3_9Test() {
         assertEquals(fro3.front3E("ab"), "ababab");
     }
 
     @Test
-    public void front3_10() {
+    public void front3_10Test() {
         assertEquals(fro3.front3E("java"), "javjavjav");
     }
 }
