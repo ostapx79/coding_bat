1b75e1339315cb544e515677529d0e96804aa690
Constantin Galus <ostapx79@gmail.com>
2023-11-15 00:41:23 +0300

    upgrade: src/main/*/DoubleX.java, тесты не проходят

--- src/main/java/edu/coding_bat/warmup_2/double_x/DoubleX.java
+++ src/main/java/edu/coding_bat/warmup_2/double_x/DoubleX.java
@@ -2,10 +2,22 @@ package edu.coding_bat.warmup_2.double_x;
 
 public class DoubleX {
     public boolean doubleX(String str) {
-        int count = 0;
-        for (int i = 0; i  < str.length() - 1; i++) {
-            if (str.startsWith("xx", i)) count++;
+
+        if (str.isEmpty() || str.length() == 1) {
+            return false;
+        } else if (str.length() < 3) {
+            if (str.startsWith("xx")) return true;
+        } else {
+            return false;
         }
-        return (count >= 1);
+
+        String subStr = str.substring(0, 2);
+        // int count = 0;
+
+        for (int i = 0; i < subStr.length(); i++) {
+            if (subStr.startsWith("xx")) return true;
+        }
+        // return !(count > 0);
+        return false;
     }
 }
