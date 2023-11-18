291565252613b1576e81660102524be427d55194
ostapx79 <ostapx79@gmail.com>
2023-11-19 02:03:34 +0300

    modified: src/main/*/Array123.java, тесты не проходят Signed-off-by: ostapx79 <ostapx79@gmail.com>
    Signed-off-by: ostapx79 <ostapx79@gmail.com>

--- src/main/java/edu/coding_bat/warmup_2/array_123/Array123.java
+++ src/main/java/edu/coding_bat/warmup_2/array_123/Array123.java
@@ -1,5 +1,7 @@
 package edu.coding_bat.warmup_2.array_123;
 
+import java.util.Arrays;
+
 public class Array123 {
     public boolean array123(int[] num) {
         /*
@@ -27,5 +29,32 @@ public class Array123 {
         res3[3] = num[4];
 
         return res1 == resArr || res2 == resArr || res3 == resArr;
+    }
+    public static int[] getCycle(int[] array)
+    {
+        for (int i=1; i<array.length-1; i++){
+            if (array.length%i!=0)
+                break;
+            int[] sub = Arrays.copyOfRange(array, 0, i);
+            if (check(array, sub))
+                return sub;
+        }
+        return array;
+    }
+    private static boolean check(int[] array, int[] sub)
+    {
+        if (array.length%sub.length!=0)
+            return false;
+        for (int k=1; k<array.length/sub.length; k++)
+            if (!isSimilar(sub, Arrays.copyOfRange(array, k*sub.length, (k+1)*sub.length)))
+                return false;
+        return true;
+    }
+    private static boolean isSimilar(int[] x, int[] y)
+    {
+        for (int i=0; i<x.length; i++)
+            if (x != y)
+                return false;
+        return true;
     }
 }
