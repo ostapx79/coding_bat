b0b6b37e682f1e4e718495e24a09252eb2b3e376
Constantin Galus <ostapx79@gmail.com>
2023-11-21 19:10:09 +0300

    modified: src/main/*/AltPairs.java and src/test/*/AltPairsTest.java, тесты проходят

--- src/main/java/edu/coding_bat/warmup_2/alt_pairs/AltPairs.java
+++ src/main/java/edu/coding_bat/warmup_2/alt_pairs/AltPairs.java
@@ -2,10 +2,36 @@ package edu.coding_bat.warmup_2.alt_pairs;
 
 public class AltPairs {
     public String altPairs(String str) {
+
         var res = "";
-        for (int i = 0; i < str.length() - 1; i += 1) {
+
+        if (str.length() == 1) res += str;
+
+        for (int i = 0; i < str.length() - 1; i += 4) {
             res += str.substring(i, i + 2);
+
         }
+
+        if (str.length() % 2 != 0 && str.length() > 3) {
+            res += str.substring(str.length() - 1);
+        }
+
         return res;
+    }
+
+    public String altPairsT(String str) {
+        String result = "";
+
+        // Run i by 4 to hit 0, 4, 8, ...
+        for (int i=0; i<str.length(); i += 4) {
+            // Append the chars between i and i+2
+            int end = i + 2;
+            if (end > str.length()) {
+                end = str.length();
+            }
+            result = result + str.substring(i, end);
+        }
+
+        return result;
     }
 }
--- src/test/java/edu/coding_bat/warmup_2/alt_pairs/AltPairsTest.java
+++ src/test/java/edu/coding_bat/warmup_2/alt_pairs/AltPairsTest.java
@@ -10,16 +10,49 @@ class AltPairsTest {
     @Test
     public void altPairs1Test() {
         assertEquals(altPairs.altPairs("kitten"), "kien");
+        assertEquals(altPairs.altPairsT("kitten"), "kien");
     }
 
     @Test
     public void altPairs2Test() {
         assertEquals(altPairs.altPairs("Chocolate"), "Chole");
+        assertEquals(altPairs.altPairsT("Chocolate"), "Chole");
     }
 
     @Test
     public void altPairs3Test() {
         assertEquals(altPairs.altPairs("CodingHorror"), "Congrr");
+        assertEquals(altPairs.altPairsT("CodingHorror"), "Congrr");
+    }
+
+    @Test
+    public void altPairs4Test() {
+        assertEquals(altPairs.altPairs("yak"), "ya");
+        assertEquals(altPairs.altPairsT("yak"), "ya");
+    }
+
+    @Test
+    public void altPairs5Test() {
+        assertEquals(altPairs.altPairs("ya"), "ya");
+        assertEquals(altPairs.altPairsT("ya"), "ya");
+    }
+
+    @Test
+    public void altPairs6Test() {
+        assertEquals(altPairs.altPairs("y"), "y");
+        assertEquals(altPairs.altPairsT("y"), "y");
+    }
+
+    @Test
+    public void altPairs7Test() {
+        assertEquals(altPairs.altPairs(""), "");
+        assertEquals(altPairs.altPairsT(""), "");
+    }
+
+    @Test
+    public void altPairs8Test() {
+        assertEquals(altPairs.altPairs("ThisThatTheOther"), "ThThThth");
+        assertEquals(altPairs.altPairsT("ThisThatTheOther"), "ThThThth");
     }
 
 }
