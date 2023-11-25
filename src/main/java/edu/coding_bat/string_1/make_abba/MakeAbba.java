895c5716b0920c6efb09ed36eba7fa771a3e0168
Constantin Galus <ostapx79@gmail.com>
2023-11-25 18:25:54 +0300

    modified: src/main/*/MakeAbba.java, тесты проходят

--- src/main/java/edu/coding_bat/string_1/make_abba/MakeAbba.java
+++ src/main/java/edu/coding_bat/string_1/make_abba/MakeAbba.java
@@ -2,6 +2,12 @@ package edu.coding_bat.string_1.make_abba;
 
 public class MakeAbba {
     public String makeAbba(String str1, String str2) {
+        /*
+        Given two strings, a and b, return the result of putting
+        them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+        Учитывая две строки, a и b, верните результат их объединения в порядке abba,
+        например. «Привет» и «Пока» возвращает «HiByeByeHi».
+         */
         return str1 + str2 + str2 + str1;
     }
 }
