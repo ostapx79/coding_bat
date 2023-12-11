4e1916d9b28a5ede48518169b0e09c7a8d57f5ff
ostapx79 <ostapx79@gmail.com>
2023-12-11 23:24:46 +0300

    modified: src/main/*/DateFashion.java Signed-off-by: ostapx79 <ostapx79@gmail.com>
    Signed-off-by: ostapx79 <ostapx79@gmail.com>

--- src/main/java/edu/coding_bat/logic_1/date_fashion/DateFashion.java
+++ src/main/java/edu/coding_bat/logic_1/date_fashion/DateFashion.java
@@ -21,6 +21,8 @@ public class DateFashion {
      */
 
     public int dateFashion(int you, int date) {
-        return 0;
+        if (you < 3 || date < 3) return 0;
+        if (you > 7 || date > 7) return 2;
+        return 1;
     }
 }
