0a7a104f34db424adebe51dc60411b49c627385b
Constantin Galus <ostapx79@gmail.com>
2024-01-02 18:01:13 +0300

    modified: str/main/*/FizzString.java scr/test/*/FizzStringTest.java, тесты проходят

--- src/main/java/edu/coding_bat/logic_1/fizz_string/FizzString.java
+++ src/main/java/edu/coding_bat/logic_1/fizz_string/FizzString.java
@@ -15,8 +15,9 @@ public class FizzString {
      */
 
     public String fizzString(String str) {
-        if (str.startsWith("f")) return "Fizz";
-        else if (str.startsWith("b") || str.endsWith("b")) return "Buzz";
-        return "FizzBuzz";
+        if (str.startsWith("f") && str.endsWith("b")) return "FizzBuzz";
+        else if (str.endsWith("b")) return "Buzz";
+        else if (str.startsWith("f")) return "Fizz";
+        return str;
     }
 }
--- src/test/java/edu/coding_bat/logic_1/fizz_string/FizzStringTest.java
+++ src/test/java/edu/coding_bat/logic_1/fizz_string/FizzStringTest.java
@@ -22,4 +22,68 @@ class FizzStringTest {
         assertEquals(fizzString.fizzString("fib"), "FizzBuzz");
     }
 
+    @Test
+    public void setFizzString4Test() {
+        assertEquals(fizzString.fizzString("abc"), "abc");
+    }
+
+    @Test
+    public void setFizzString5Test() {
+        assertEquals(fizzString.fizzString("fooo"), "Fizz");
+    }
+
+    @Test
+    public void setFizzString6Test() {
+        assertEquals(fizzString.fizzString("booo"), "booo");
+    }
+
+    @Test
+    public void setFizzString7Test() {
+        assertEquals(fizzString.fizzString("ooob"), "Buzz");
+    }
+
+    @Test
+    public void setFizzString8Test() {
+        assertEquals(fizzString.fizzString("fooob"), "FizzBuzz");
+    }
+
+    @Test
+    public void setFizzString9Test() {
+        assertEquals(fizzString.fizzString("f"), "Fizz");
+    }
+
+    @Test
+    public void setFizzString10Test() {
+        assertEquals(fizzString.fizzString("b"), "Buzz");
+    }
+
+    @Test
+    public void setFizzString11Test() {
+        assertEquals(fizzString.fizzString("abcb"), "Buzz");
+    }
+
+    @Test
+    public void setFizzString13Test() {
+        assertEquals(fizzString.fizzString("Hello"), "Hello");
+    }
+
+    @Test
+    public void setFizzString14Test() {
+        assertEquals(fizzString.fizzString("Hellob"), "Buzz");
+    }
+
+    @Test
+    public void setFizzString15Test() {
+        assertEquals(fizzString.fizzString("af"), "af");
+    }
+
+    @Test
+    public void setFizzString16Test() {
+        assertEquals(fizzString.fizzString("bf"), "bf");
+    }
+
+    @Test
+    public void setFizzString17Test() {
+        assertEquals(fizzString.fizzString("fb"), "FizzBuzz");
+    }
 }
