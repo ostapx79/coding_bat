5a63d5ad9531dab4752b21a0e0c49f3e03d5f47e
Constantin Galus <ostapx79@gmail.com>
2024-01-10 00:07:32 +0300

    modified: str/main/*/InOrder.java, тесты не проходят

--- src/main/java/edu/coding_bat/logic_1/in_order/InOrder.java
+++ src/main/java/edu/coding_bat/logic_1/in_order/InOrder.java
@@ -12,7 +12,12 @@ public class InOrder {
      */
 
     public boolean inOrder(int a, int b, int c, boolean bOk) {
-        if (a < b && b < c || bOk) return true;
+        // 4, 2, 2, true -> false
+        // 7, 5, 4, true -> false
+        if ((a > b || b < c) && bOk) return true;
+        else if ((a > b || b >= c) && bOk) return false;
+        else if (a < b && b < c || bOk) return true;
+
         return false;
     }
 }
