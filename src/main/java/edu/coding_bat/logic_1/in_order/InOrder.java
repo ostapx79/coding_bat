449e3b53090412ee9c22f01370aa91a787a971a2
ostapx79 <ostapx79@gmail.com>
2024-01-10 01:00:51 +0300

    modified: src/main/*/InOrder.java, тесты не проходят Signed-off-by: ostapx79 <ostapx79@gmail.com>
    Signed-off-by: ostapx79 <ostapx79@gmail.com>

--- src/main/java/edu/coding_bat/logic_1/in_order/InOrder.java
+++ src/main/java/edu/coding_bat/logic_1/in_order/InOrder.java
@@ -12,11 +12,12 @@ public class InOrder {
      */
 
     public boolean inOrder(int a, int b, int c, boolean bOk) {
-        // 4, 2, 2, true -> false
-        // 7, 5, 4, true -> false
-        if ((a > b || b < c) && bOk) return true;
-        else if ((a > b || b >= c) && bOk) return false;
-        else if (a < b && b < c || bOk) return true;
+        // 4, 5, 2, true -> false
+        if (a <= b) {
+            if (b < c || bOk) return true;
+        } else if (a > b) {
+            if (b < c && bOk) return true;
+        }
 
         return false;
     }
