00ef1f53267e725632870c9d72b2025f85dca2bb
Constantin Galus <ostapx79@gmail.com>
2024-01-20 17:27:47 +0300

    modified: str/main/*/LastDigit.java and src/test/*/LastDigitTest.java, тесты проходят

--- src/main/java/edu/coding_bat/logic_1/last_digit/LastDigit.java
+++ src/main/java/edu/coding_bat/logic_1/last_digit/LastDigit.java
@@ -13,6 +13,6 @@ public class LastDigit {
      */
 
     public boolean lastDigit(int a, int b, int c) {
-        return false;
+        return (a % 10 == b % 10 || b % 10 == c % 10 || a % 10 == c % 10);
     }
 }
--- src/test/java/edu/coding_bat/logic_1/last_digit/LastDigitTest.java
+++ src/test/java/edu/coding_bat/logic_1/last_digit/LastDigitTest.java
@@ -22,4 +22,53 @@ class LastDigitTest {
         assertTrue(lastDigit.lastDigit(23, 19, 3));
     }
 
+    @Test
+    public void setLastDigit4Test() {
+        assertTrue(lastDigit.lastDigit(23, 19, 39));
+    }
+
+    @Test
+    public void setLastDigit5Test() {
+        assertFalse(lastDigit.lastDigit(1, 2, 3));
+    }
+
+    @Test
+    public void setLastDigit6Test() {
+        assertTrue(lastDigit.lastDigit(1, 1, 2));
+    }
+
+    @Test
+    public void setLastDigit7Test() {
+        assertTrue(lastDigit.lastDigit(1, 2, 2));
+    }
+
+    @Test
+    public void setLastDigit8Test() {
+        assertFalse(lastDigit.lastDigit(14, 25, 43));
+    }
+
+    @Test
+    public void setLastDigit9Test() {
+        assertTrue(lastDigit.lastDigit(14, 25, 45));
+    }
+
+    @Test
+    public void setLastDigit10Test() {
+        assertFalse(lastDigit.lastDigit(248, 106, 1002));
+    }
+
+    @Test
+    public void setLastDigit11Test() {
+        assertTrue(lastDigit.lastDigit(248, 106, 1008));
+    }
+
+    @Test
+    public void setLastDigit12Test() {
+        assertTrue(lastDigit.lastDigit(10, 11, 20));
+    }
+
+    @Test
+    public void setLastDigit13Test() {
+        assertTrue(lastDigit.lastDigit(0, 11, 0));
+    }
 }
