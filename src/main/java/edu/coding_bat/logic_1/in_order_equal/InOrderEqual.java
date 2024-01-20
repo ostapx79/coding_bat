79f37a4d83308351e005369eed5a3bdd8201093d
Constantin Galus <ostapx79@gmail.com>
2024-01-20 17:04:04 +0300

    modified: str/main/*/InOrder.java, тесты не проходят

--- src/main/java/edu/coding_bat/logic_1/in_order_equal/InOrderEqual.java
+++ src/main/java/edu/coding_bat/logic_1/in_order_equal/InOrderEqual.java
@@ -15,7 +15,6 @@ public class InOrderEqual {
      */
 
     public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
-        if (a > b || b > c && equalOk) return false;
         if (a < b && b < c || equalOk) return true;
         return false;
     }
