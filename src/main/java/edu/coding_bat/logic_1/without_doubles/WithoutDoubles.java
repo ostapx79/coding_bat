ca896c7e3fed9923f92108667147b92da13ad092
Constantin Galus <ostapx79@gmail.com>
2024-01-21 18:40:17 +0300

    modified: str/main/*/WithoutDoubles.java, тесты проходят

--- src/main/java/edu/coding_bat/logic_1/without_doubles/WithoutDoubles.java
+++ src/main/java/edu/coding_bat/logic_1/without_doubles/WithoutDoubles.java
@@ -17,6 +17,7 @@ public class WithoutDoubles {
 
     public int withoutDoubles(int die1, int die2, boolean noDoubles) {
         if (die1 == die2) {
+            if (die1 == 6 && die2 == 6 && noDoubles) return 7;
             if (noDoubles) return die1 + die2 + 1;
             else return die1 + die2;
         }
