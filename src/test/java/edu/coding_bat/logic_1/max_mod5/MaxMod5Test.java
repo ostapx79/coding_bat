ae18779fc308fd898ba92f8d7ebe331bc3b30c60
Constantin Galus <ostapx79@gmail.com>
2024-02-02 13:02:55 +0300

    modified: str/main/*/WithoutDoubles.java and WithoutDoublesTest.java, тесты проходят

---
+++ src/test/java/edu/coding_bat/logic_1/max_mod5/MaxMod5Test.java
@@ -0,0 +1,64 @@
+package edu.coding_bat.logic_1.max_mod5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxMod5Test {
+    MaxMod5 maxMod5 = new MaxMod5();
+
+    @Test
+    public void setMaxMod5_1Test() {
+        assertEquals(maxMod5.maxMod5(2, 3), 3);
+    }
+
+    @Test
+    public void setMaxMod5_2Test() {
+        assertEquals(maxMod5.maxMod5(6, 2), 6);
+    }
+
+    @Test
+    public void setMaxMod5_3Test() {
+        assertEquals(maxMod5.maxMod5(3, 2), 3);
+    }
+
+    @Test
+    public void setMaxMod5_4Test() {
+        assertEquals(maxMod5.maxMod5(8, 12), 12);
+    }
+
+    @Test
+    public void setMaxMod5_5Test() {
+        assertEquals(maxMod5.maxMod5(7, 12), 7);
+    }
+
+    @Test
+    public void setMaxMod5_6Test() {
+        assertEquals(maxMod5.maxMod5(11, 6), 6);
+    }
+
+    @Test
+    public void setMaxMod5_7Test() {
+        assertEquals(maxMod5.maxMod5(2, 7), 2);
+    }
+
+    @Test
+    public void setMaxMod5_8Test() {
+        assertEquals(maxMod5.maxMod5(7, 7), 0);
+    }
+
+    @Test
+    public void setMaxMod5_9Test() {
+        assertEquals(maxMod5.maxMod5(9, 1), 9);
+    }
+
+    @Test
+    public void setMaxMod5_10Test() {
+        assertEquals(maxMod5.maxMod5(9, 14), 9);
+    }
+
+    @Test
+    public void setMaxMod5_Test() {
+        assertEquals(maxMod5.maxMod5(1, 2), 2);
+    }
+}
